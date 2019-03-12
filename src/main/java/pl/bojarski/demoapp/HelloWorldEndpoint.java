a7105e5ab3008023ea962d2dd3a993d9f0e9dc38
Kamil Bojarski <bojarski.kamil96@gmail.com>
2019-03-12 09:20:04 +0100

    init repo

---
+++ src/main/java/pl/bojarski/demoapp/HelloWorldEndpoint.java
@@ -0,0 +1,15 @@
+package pl.bojarski.demoapp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@RestController
+public class HelloWorldEndpoint {
+
+    @RequestMapping(method = GET, path = "hello")
+    public String hello() {
+        return "Hello World!";
+    }
+}
